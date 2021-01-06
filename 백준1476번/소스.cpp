444931d0f1d957309b228e188b6194b39e6889ea
Hongsoojeong <my_youth99@naver.com>
2021-01-06 19:40:48 +0900

    BOJ 1476

---
+++ 백준1476번/소스.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int e; int s; int m;
+	int year = 1;
+	cin >> e >> s >> m;
+	while (1) {
+		if ((year - e) % 15 == 0 && (year - s) % 28 == 0 && (year - m) % 19 == 0)
+		{
+			cout << year;
+			break;
+		}
+		year++;
+	}
+
+	return 0;
+}
